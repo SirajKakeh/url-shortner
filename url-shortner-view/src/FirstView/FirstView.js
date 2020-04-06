1a26db6bfdbd7bf3e0d6c3ac2f2f5ab670a87944
farah abuesbaa <farahabuebsaa33@gmail.com>
2020-04-06 11:21:35 +0300

    main page design

--- url-shortner-view/src/FirstView/FirstView.js
+++ url-shortner-view/src/FirstView/FirstView.js
@@ -1,18 +1,21 @@
 import React ,{Component} from 'react';
 import classes from './FirstView.module.css';
-import Auxiliary from '../Auxiliary/Auxiliary';
+
+
 class FirstView extends Component{
     render(){
         return(
-            <div className={classes.FirstView}>
-                <h1>Paste the URL to be shortened</h1>
-                <div className={classes.InputDiv}>
-                    <input className={classes.Input} placeholder="Enter the link here"/>
-                    <button className={classes.Button}>Shorten URL</button>
-                </div>
-                <div className={classes.Div}>
-                    <p>ShortURL.at is a free tool to shorten a URL or reduce a link.</p>
-                    <p>Use our URL Shortener to create a shortened link making it easy to remember.</p>
+            <div className={classes.MainDiv} >
+                <div className={classes.FirstView}>
+                    <h1>Paste the URL to be shortened</h1>
+                    <div className={classes.InputDiv}>
+                        <input className={classes.Input} placeholder="Enter the link here"/>
+                        <button className={classes.Button}>Shorten URL</button>
+                    </div>
+                    <div className={classes.Div}>
+                        <p>ShortURL.at is a free tool to shorten a URL or reduce a link.</p>
+                        <p>Use our URL Shortener to create a shortened link making it easy to remember.</p>
+                    </div>
                 </div>
             </div>
         );
