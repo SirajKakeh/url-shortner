65ac3d4a3cba7c57e363dbf6ff1fdedc22bc7261
Siraj Kakeh <seraj.kakah@ww.com>
2020-04-07 02:29:46 +0300

    WiP

--- server/.flaskenv
+++ server/.flaskenv
@@ -1,1 +1,1 @@
-FLASK_APP=server.py
+FLASK_APP=mu.py
--- server/app/routes.py
+++ server/app/routes.py
@@ -3,4 +3,5 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    # return render_template('index.html')
+    return "hi"
