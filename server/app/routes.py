e2e170895b1107e948a1a5748c6be745c26576e8
Siraj Kakeh <sirajonline.89@gmail.com>
2020-04-12 02:20:00 +0300

    fix id

--- server/app/routes.py
+++ server/app/routes.py
@@ -40,4 +40,4 @@ def encode_url():
 
     short_url_id = db.insert_one({"url": url, "short_url": short_url}).inserted_id
 
-    return make_response({"id": 'str(short_url_id)', "short_url": base_url + '/' + short_url}, 201)
+    return make_response({"id": str(short_url_id), "short_url": base_url + '/' + short_url}, 201)
