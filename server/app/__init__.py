496f169bb914edd426dfadc3acb1c42bfce85d26
Siraj Kakeh <sirajonline.89@gmail.com>
2020-04-13 17:32:44 +0300

    cors fix

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from pymongo import MongoClient
+from flask_cors import CORS
 import os
 
 from config import Config
 
 app = Flask(__name__)
-
+CORS(app)
 app.config.from_object(Config)
 
 client = MongoClient(os.environ.get("url_shortner_db_string"))
