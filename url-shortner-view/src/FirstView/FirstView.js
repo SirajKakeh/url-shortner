44b7ee57a515a2c0e05b3ad05ffe5f2ffb513bf4
farah abuesbaa <farahabuebsaa33@gmail.com>
2020-04-14 17:59:57 +0300

    FE-Use-Prettier-on-React-code

--- url-shortner-view/src/FirstView/FirstView.js
+++ url-shortner-view/src/FirstView/FirstView.js
@@ -1,23 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './FirstView.module.css';
+import PostUrl from '../Service';
 
-const FirstView = () => (
-	<div className={classes.MainDiv}>
-		<div className={classes.FirstView}>
-			<h1>Paste the URL to be shortened</h1>
-			<div className={classes.InputDiv}>
-				<input className={classes.Input} placeholder='Enter the link here' />
-				<button className={classes.Button}>Shorten URL</button>
-			</div>
-			<div className={classes.Div}>
-				<p>ShortURL.at is a free tool to shorten a URL or reduce a link.</p>
-				<p>
-					Use our URL Shortener to create a shortened link making it easy to
-					remember.
-				</p>
+const FirstView = () => {
+	const [url, setUrl] = useState('');
+	// useEffect(() => {});
+
+	const sendUrlHandler = (url) => {
+		if (url !== '') {
+			// console.log({ setUrl });
+			console.log(PostUrl(url));
+		}
+	};
+
+	// const update = (event) => {
+	// 	event.preventDefault();
+	// 	setUrl(event.target.value);
+	// 	// console.log(setUrl);
+	// };
+	return (
+		<div className={classes.MainDiv}>
+			<div className={classes.FirstView}>
+				<h1> Paste the URL to be shortened </h1>
+				<div className={classes.InputDiv}>
+					<input
+						className={classes.Input}
+						placeholder='Enter the link here'
+						onChange={(event) => {
+							event.preventDefault();
+							setUrl(event.target.value);
+						}}
+					/>
+					<button
+						className={classes.Button}
+						onClick={() => sendUrlHandler(url)}
+					>
+						Shorten URL
+					</button>
+				</div>
+				<div className={classes.Div}>
+					<p> ShortURL.at is a free tool to shorten a URL or reduce a link. </p>
+					<p>
+						Use our URL Shortener to create a shortened link making it easy to
+						remember
+					</p>
+				</div>
 			</div>
 		</div>
-	</div>
-);
-
+	);
+};
 export default FirstView;
--- url-shortner-view/src/Header/Header.js
+++ url-shortner-view/src/Header/Header.js
@@ -3,7 +3,7 @@ import classes from './Header.module.css';
 
 const Header = () => (
 	<header className={classes.Header}>
-		<p> Short URL </p>{' '}
+		<p> Short URL </p>
 	</header>
 );
 
