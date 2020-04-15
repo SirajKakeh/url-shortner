cd72fe8439452836a1942828229b892fafa0ec3a
farah abuesbaa <farahabuebsaa33@gmail.com>
2020-04-15 15:33:23 +0300

    share url section design

--- url-shortner-view/src/FirstView/FirstView.js
+++ url-shortner-view/src/FirstView/FirstView.js
@@ -1,20 +1,23 @@
 import React, { useState } from 'react';
 import classes from './FirstView.module.css';
 import PostUrl from '../Service';
+import SecondView from '../SecondView/SecondView';
 
 const FirstView = () => {
 	const [url, setUrl] = useState('');
 
 	const sendUrlHandler = (url) => {
 		if (url !== '') {
-			PostUrl(url);
+			PostUrl(url).then((response) => {
+				console.log(response.data.short_url);
+			});
 		}
 	};
 
 	return (
 		<div className={classes.MainDiv}>
 			<div className={classes.FirstView}>
-				<h1> Paste the URL to be shortened </h1>
+				<h1> Paste the URL to be shortened </h1>{' '}
 				<div className={classes.InputDiv}>
 					<input
 						className={classes.Input}
@@ -23,22 +26,22 @@ const FirstView = () => {
 							event.preventDefault();
 							setUrl(event.target.value);
 						}}
-					/>
+					/>{' '}
 					<button
 						className={classes.Button}
 						onClick={() => sendUrlHandler(url)}
 					>
-						Shorten URL
-					</button>
-				</div>
+						Shorten URL{' '}
+					</button>{' '}
+				</div>{' '}
 				<div className={classes.Div}>
-					<p> ShortURL.at is a free tool to shorten a URL or reduce a link. </p>
+					<p> ShortURL.at is a free tool to shorten a URL or reduce a link. </p>{' '}
 					<p>
 						Use our URL Shortener to create a shortened link making it easy to
-						remember
-					</p>
-				</div>
-			</div>
+						remember{' '}
+					</p>{' '}
+				</div>{' '}
+			</div>{' '}
 		</div>
 	);
 };
---
+++ url-shortner-view/src/ShareUrlSection/ShareUrlSection.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import classes from './ShareUrlSection.module.css';
+import ShareUrlLinks from './ShareUrlLinks/ShareurlLinks';
+const ShareUrlSection = () => (
+	<div className={classes.ShareUrlDiv}>
+		<div className={classes.ShareUrlSectionDiv}>
+			<h2> Share URL </h2>
+			<ul>
+				<ShareUrlLinks />
+			</ul>
+		</div>{' '}
+	</div>
+);
+export default ShareUrlSection;
